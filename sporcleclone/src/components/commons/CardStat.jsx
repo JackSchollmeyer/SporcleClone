db04545281abdce4d0ad2ad4654b8bdf2fa52a7a
Jack Schollmeyer <jack@schollmeyer.com>
2021-10-07 18:33:57 -0500

    finished random number generator for tickets. percentage still in progress

--- sporcleclone/src/components/commons/CardStat.jsx
+++ sporcleclone/src/components/commons/CardStat.jsx
@@ -8,27 +8,25 @@ import {
 
 export default function CardStat({ title, current, last, change }) {
   const setIcon =
-    change === "0%" ? (
-      <MinusSmIcon className={s.minusIcon} />
-    ) : change === "-5%" ? (
+    change < 0 ? (
       <ArrowSmDownIcon className={s.downArrow} />
-    ) : change === "2%" ? (
+    ) : change > 0 ? (
       <ArrowSmUpIcon className={s.upArrow} />
     ) : (
-      ""
+      <MinusSmIcon className={s.minusIcon} />
     );
 
   const changeStyled =
-    change === "-5%"
+    change < 0
       ? { backgroundColor: "rgba(250, 168, 168, 0.3)" }
-      : change === "2%"
+      : change > 0
       ? { backgroundColor: "rgba(7, 161, 59, 0.2)" }
       : { backgroundColor: "rgb(250, 250, 250)" };
 
   const changeTextStyled =
-    change === "-5%"
+    change < 0
       ? { color: "rgb(180, 50, 50)" }
-      : change === "2%"
+      : change > 0
       ? { color: "rgb(4, 119, 43)" }
       : { color: "black" };
 
@@ -37,12 +35,12 @@ export default function CardStat({ title, current, last, change }) {
       <h3>{title}</h3>
       <div className={s.data}>
         <p>
-          <span>{current}</span>
-          {last}
+          <span>{current} </span>
+          from {last}
         </p>
         <div className={s.percentage} style={changeStyled}>
           {setIcon}
-          <p style={changeTextStyled}>{change}</p>
+          <p style={changeTextStyled}>{change} %</p>
         </div>
       </div>
     </div>
--- sporcleclone/src/pages/index.js
+++ sporcleclone/src/pages/index.js
@@ -4,17 +4,48 @@ import { ArrowDownIcon, MinusIcon } from "@heroicons/react/outline";
 import CardStat from "../components/commons/CardStat";
 
 export default function Tickets() {
+  const randomNumbers = (maxNum) => {
+    return {
+      currentStat: Math.floor(Math.random() * maxNum),
+      lastStat: Math.floor(Math.random() * maxNum),
+    };
+  };
+
+  const completedStats = randomNumbers(150);
+  const erasedStats = randomNumbers(150);
+  const quizStats = randomNumbers(150);
+  const patientStats = randomNumbers(150);
+
+  // const percentage = Math.floor(
+  //   (completedStats.currentStat / completedStats.lastStat - 1) * 100
+  // );
+
   return (
     <div className={s.grid}>
       <CardStat
         title="Completed Quizzes"
-        current={0}
-        last=" from 0"
-        change="0%"
+        current={completedStats.currentStat}
+        last={completedStats.lastStat}
+        change={percentage}
       />
-      <CardStat title="Erased Quizzes" current={0} last=" from 0" change="0%" />
-      <CardStat title="Quizzes" current={122} last=" from 129" change="-5%" />
-      <CardStat title="Patient" current={127} last=" from 124" change="2%" />
+      <CardStat
+        title="Erased Quizzes"
+        current={erasedStats.currentStat}
+        last={erasedStats.lastStat}
+        change={}
+      />
+      <CardStat
+        title="Quizzes"
+        current={quizStats.currentStat}
+        last={quizStats.lastStat}
+        change={}
+      />
+      <CardStat
+        title="Patient"
+        current={patientStats.currentStat}
+        last={patientStats.lastStat}
+        change={}
+      />
     </div>
   );
 }
